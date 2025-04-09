c3b8b03841acda7efb95096116bab911e7081ef9
Jhonatan Moreira <jhonathanm836@gmail.com>
2025-04-09 20:59:46 -0300

    Fix eslint and add storybook

--- jest.config.js
+++ jest.config.js
@@ -5,11 +5,37 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.ts(x)?',
     '!src/app/**',
-    '!src/lib/registry.tsx'
+    '!src/lib/registry.tsx',
+    '!src/types/**',
+    '!src/styles/global.ts' // resolve o problema
   ],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  setupFilesAfterEnv: [
+    '<rootDir>/.jest/setup.ts',
+    '@testing-library/jest-dom',
+    'jest-styled-components'
+  ],
   modulePaths: ['<rootDir>/src/'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      'babel-jest',
+      {
+        presets: [
+          [
+            'next/babel',
+            {
+              'preset-react': {
+                runtime: 'automatic' // Keep this for React 19
+                // Remove importSource if not using Emotion
+              }
+            }
+          ]
+        ]
+        // Remove the plugins array entirely
+      }
+    ]
+  },
+  moduleNameMapper: {
+    '^styled-components':
+      'styled-components/dist/styled-components.browser.cjs.js'
   }
 }
